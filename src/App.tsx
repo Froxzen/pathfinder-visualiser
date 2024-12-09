23f95c98a095f2da12d45f4467b229a0e13b8a1e
Froxzen <harvardchong1@gmail.com>
2024-12-10 00:26:20 +1100

    Added draw walls functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import { useRef } from "react";
 import { Grid } from "./components/Grid";
 import { PathfindingProvider } from "./context/PathfindingContext";
 import { SpeedProvider } from "./context/SpeedContext";
 import { TileProvider } from "./context/TileContext";
 
 function App() {
+	const isVisualisationRunningRef = useRef(false);
+
 	return (
 		<PathfindingProvider>
 			<TileProvider>
 				<SpeedProvider>
 					<div className="h-screen w-screen flex flex-col">
-						<Grid/>
+						<Grid isVisualisationRunningRef={isVisualisationRunningRef} />
 					</div>
 				</SpeedProvider>
 			</TileProvider>
@@ -18,3 +21,4 @@ function App() {
 }
 
 export default App;
+// 38:39
